74c668530ab3a2a993199f0d1bd9e4cdc3f15416
yaacine <gy_zidelmal@esi.dz>
2020-06-21 11:18:36 +0100

    working on country DAO

---
+++ app/src/main/java/com/example/projettdm/DataManager/Dao/CountryDAO.kt
@@ -0,0 +1,33 @@
+package com.example.projettdm.DataManager.Dao
+
+import androidx.room.*
+import com.example.projettdm.DataManager.Entities.Country
+
+
+@Dao
+interface CountryDAO {
+
+
+    @Query("SELECT * FROM country")
+    fun getAll(): List<Country>
+
+    @Query ("SELECT id, name , code, visited, favorite FROM country")
+    fun getAllAbstracts(): List<Country>
+
+
+    @Query ("SELECT id, name , code, visited, favorite FROM country")
+    fun getFavoriteAbstracts(): List<Country>
+
+
+    @Query("SELECT * FROM country WHERE code LIKE :code")
+    fun findBycode(code: String): Country
+
+    @Insert
+    fun insertAll(vararg country: Country)
+
+    @Delete
+    fun delete(country: Country)
+
+    @Update
+    fun updateCountry(vararg country: Country)
+}
---
+++ app/src/main/java/com/example/projettdm/DataManager/Entities/Country.kt
@@ -0,0 +1,18 @@
+package com.example.projettdm.DataManager.Entities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "country")
+data class Country (
+    @PrimaryKey(autoGenerate = true) var id: Int,
+    @ColumnInfo(name = "name") var name: String,
+    @ColumnInfo(name = "code") var code: String,
+    @ColumnInfo(name = "description") var description: String,
+    @ColumnInfo(name = "hymeSrc") var hymeSrc: String,
+    @ColumnInfo(name = "flagSrc") var flagSrc: String,
+    @ColumnInfo(name = "visited") var visted: Boolean,
+    @ColumnInfo(name = "favorite") var favorite: Boolean
+
+)
