1753a1c7134db1880c3c50c51892c485689f200a
yaacine <gy_zidelmal@esi.dz>
2020-06-26 13:34:34 +0100

    working on appDrawer , main is fragment now

--- app/src/main/java/com/example/projettdm/MainActivity.kt
+++ app/src/main/java/com/example/projettdm/MainActivity.kt
@@ -3,15 +3,27 @@ package com.example.projettdm
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.room.Room
 import com.example.projettdm.DataManager.AppDatabase
 import com.example.projettdm.DataManager.Entities.Country
 import com.example.projettdm.DataManager.Entities.Image
+import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() ,NavigationView.OnNavigationItemSelectedListener {
+
+    lateinit var toolbar: Toolbar
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var navView: NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +34,44 @@ class MainActivity : AppCompatActivity() {
 
         populateDatabase(db)
 
+
+
+        toolbar = findViewById(R.id.toolbar)
+
+        setSupportActionBar(toolbar)
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navView = findViewById(R.id.nav_view)
+
+        val toggle = ActionBarDrawerToggle(
+            this, drawerLayout, toolbar, 0, 0
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        navView.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_profile -> {
+                Toast.makeText(this, "Profile clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_messages -> {
+                Toast.makeText(this, "Messages clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_friends -> {
+                Toast.makeText(this, "Friends clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_update -> {
+                Toast.makeText(this, "Update clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_logout -> {
+                Toast.makeText(this, "Sign out clicked", Toast.LENGTH_SHORT).show()
+            }
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
 
         /*
           GlobalScope.launch {
@@ -42,16 +92,18 @@ class MainActivity : AppCompatActivity() {
             }
         }
         */
-        val iconName = resources.getResourceEntryName(R.drawable.ic_launcher_background)
-        println("icon name ===>"+ iconName)
 
 
-        button.setOnClickListener {
+      //  val iconName = resources.getResourceEntryName(R.drawable.ic_launcher_background)
+        //println("icon name ===>"+ iconName)
+
+
+        /*button.setOnClickListener {
 
             val intent = Intent(this, CountryDetails::class.java)
             startActivity(intent)
-        }
-    }
+        }*/
+
 
 
 
